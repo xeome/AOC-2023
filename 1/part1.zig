f3ffe6d2978fdd636b1b27447f82a63ef7cfc262
Emin <xeome@proton.me>
2023-12-02 22:15:36 +0300

    Day01 part01

---
+++ 1/part1.zig
@@ -0,0 +1,49 @@
+const std = @import("std");
+
+pub fn main() void {
+    const text =
+        \\1abc2
+        \\pqr3stu8vwx
+        \\a1b2c3d4e5f
+        \\treb7uchet 
+    ;
+
+    const sum = part1(text);
+    std.debug.print("sum: {}\n", .{sum});
+}
+
+// Concatenates the first and last digit of each line of text. and sums the result from each line.
+fn part1(text: []const u8) u32 {
+    var sum: u32 = 0;
+    var first: u8 = 0;
+    var last: u8 = 0;
+
+    var lines = std.mem.split(u8, text, "\n");
+    while (lines.next()) |line| {
+        first = 0;
+        last = 0;
+        if (line.len == 0) {
+            continue;
+        }
+
+        var i: usize = 0;
+        while (i < line.len) : (i += 1) {
+            if (std.ascii.isDigit(line[i])) {
+                first = line[i] - '0';
+                break;
+            }
+        }
+
+        i = line.len - 1;
+        while (i > 0) : (i -= 1) {
+            if (std.ascii.isDigit(line[i])) {
+                last = line[i] - '0';
+                break;
+            }
+        }
+
+        sum += first * 10 + last;
+    }
+
+    return sum;
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# AOC 2023 zig solutions
+
+Usage:
+
+```sh
+zig run 1/part1.zig
+```
+
+change path to run other parts.
+
+> [!WARNING]
+> This is my first time using zig, so the code is probably not idiomatic zig.  
